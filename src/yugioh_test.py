ed8a239adeddf9d5dc03312346f5f6cba0e03d2f
Beatriz Gutiérrez Arazo <beagutara@alum.us.es>
2022-10-06 18:49:05 +0200

    YuGiOh

--- src/yugioh_test.py
+++ src/yugioh_test.py
@@ -3,4 +3,6 @@ from yugioh import *
 if __name__ == '__main__':
 # Test de la función lee_cartas
       Cartas = lee_cartas('./data/card_data_FULL.csv')
-      print(Cartas[:-3])
+      print(len(Cartas))
+      print(Cartas[:3])
+      print(Cartas[-3:])
