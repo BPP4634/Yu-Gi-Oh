4946ff603aed26ed07fd5f4786a00f2cd0bb7fad
Beatriz Gutiérrez Arazo <beagutara@alum.us.es>
2022-11-04 16:19:47 +0100

    YuGiOh

--- src/yugioh.py
+++ src/yugioh.py
@@ -37,8 +37,8 @@ def numero_de_atributos(Cartas):
             atributos.add(carta.Attribute)
     #Se borra " " del conjunto, ya que ha sido añadido porque algunas cartas no tienen dicho valor
     atributos.remove('')
-    #Como resultado, la función devuelve los atributos y cuantos hay.
-    return atributos, len(atributos)
+    #Como resultado, la función devuelve cuantos atributos hay y cuales son.
+    return len(atributos),atributos
 
 def existe_cartas_mayores_que_ataque_dado(Cartas, ataque):
     #Se comprueba si existe alguna carta con un ataque mayor que el dado
@@ -50,24 +50,23 @@ def existe_cartas_mayores_que_ataque_dado(Cartas, ataque):
     return False
 
 def valor_maximo_defensa_en_atributos_dados(Cartas, atributos):
-    cartasatributo = []
+    result = []
     #Añade cartas a la lista "cartasatributo" cuyo atributo sea igual a uno de los atributos dados
     for carta in Cartas:
-        for atributo in atributos:
-            if carta.Attribute == atributo:
-                cartasatributo.append(carta)
+        if carta.Attribute in atributos:
+            result.append(carta)
     #Ordena las cartas de dicha lista de mayor a menor defensa
-    cartasatributo = sorted(cartasatributo, key=lambda at : at[6], reverse=True)
+    result = max(result, key=lambda at : at[6])
     #Devuelve la defensa, el nombre y el atributo de la primera carta
-    return (cartasatributo[0][6],cartasatributo[0][0],cartasatributo[0][4])
+    return (result[6],result[0],result[4])
 
 def calcula_n_cartas_maximas_suertes_de_raza(Cartas,raza):
-    carmaxsuer = []
+    result = []
     #Añade a la lista "carmaxsuer" cartas cuya raza sea la misma que la dada
     for carta in Cartas:
         if carta.Race == raza:
-            carmaxsuer.append(carta)
+            result.append(carta)
     #Ordena las cartas de dicha lista de mayor a menor suerte
-    carmaxsuer = sorted(carmaxsuer, key=lambda lc : lc[7], reverse=True)
+    result = sorted(result, key=lambda lc : lc[7], reverse=True)
     #Devuelve la lista
-    return carmaxsuer
+    return result
--- src/yugioh_test.py
+++ src/yugioh_test.py
@@ -1,39 +1,40 @@
 from yugioh import *
 
 def main():
-      Cartas = test_lee_cartas('./data/card_data_FULL.csv')
-      test_numero_de_atributos(Cartas)
-      test_existe_cartas_mayores_que_ataque_dado(Cartas,4000)
-      test_valor_maximo_defensa_en_atributos_dados(Cartas, ['DARK','EART','FIRE'])
-      test_calcula_n_cartas_maximas_suertes_de_raza(Cartas,'Zombie')
+      CARTAS = test_lee_cartas('./data/card_data_FULL.csv')
+      test_numero_de_atributos(CARTAS)
+      test_existe_cartas_mayores_que_ataque_dado(CARTAS,4000)
+      test_valor_maximo_defensa_en_atributos_dados(CARTAS, ['FIRE','WIND','WATER'])
+      test_calcula_n_cartas_maximas_suertes_de_raza(CARTAS,'Zombie',5)
 
 # Test de la función lee_cartas
 def test_lee_cartas(archivo):
       Cartas = lee_cartas(archivo)
-      print(len(Cartas))
-      print(Cartas[:3])
-      print(Cartas[-3:])
+      print("Hay información de", len(Cartas), 'cartas en el dataset.')
+      print("Las tres primeras son:", Cartas[:3])
+      print("Las tres últimas son:", Cartas[-3:])
       return Cartas
 
 # Test de la función numero_de_atributos
 def test_numero_de_atributos(Cartas):
       att = numero_de_atributos(Cartas)
-      print('Existen', att[1], 'atributos, los cuales son:', att[0])
+      print('Existen {} atributos, los cuales son: {}'.format(*att))
 
 # Test de la función existe_cartas_mayores_que_ataque_dado
 def test_existe_cartas_mayores_que_ataque_dado(Cartas, atk):
       tecmqad = existe_cartas_mayores_que_ataque_dado(Cartas,atk)
-      print('¿Existen cartas con un ataque mayor que ' + str(atk) + '?', tecmqad)
+      print('¿Existen cartas con un ataque mayor que {}? {}'.format(atk,tecmqad))
 
 # Test de la función valor_maximo_defensa_en_atributos_dados
 def test_valor_maximo_defensa_en_atributos_dados(Cartas, atributos):
       vmdead = valor_maximo_defensa_en_atributos_dados(Cartas, atributos)
-      print('El valor máximo de la defensa de una carta de los atributos propuestos es ' + str(vmdead[0]) + ', de la carta ' + vmdead[1] + ', cuyo atributo es', vmdead[2])
+      print('El valor máximo de la defensa de una carta de los atributos propuestos es {}, de la carta {},\
+ cuyo atributo es {}'.format(*vmdead))
 
 # Test de la función calcula_n_cartas_maximas_suertes_de_raza
-def test_calcula_n_cartas_maximas_suertes_de_raza(Cartas,raza,n=10):
+def test_calcula_n_cartas_maximas_suertes_de_raza(Cartas,raza,n=3):
       cmsdr = calcula_n_cartas_maximas_suertes_de_raza(Cartas,raza)
-      print('Las', n, 'cartas de raza', raza, 'con mas suerte son:', cmsdr[:n])
+      print('Las {} cartas de raza {} con mas suerte son: {}'.format(n,raza,cmsdr[:n]))
 
 if __name__ == '__main__':
       main()
